e19cbbc3431736cadb975f87925bf652e7fb52c8
petrol <pietrogouvearepleop@gmail.com>
2026-02-18 15:17:08 -0300

    pequenos ajustes

--- app.ts
+++ app.ts
@@ -4,13 +4,18 @@ import userRouter from './routes/user.routes.ts';
 import subscriptionRouter from './routes/subscription.routes.ts';
 import authRouter from './routes/auth.routes.ts';
 import { connectToDatabase } from './database/postgres.ts';
+import { errorMiddleware } from './middlewares/error.middleware.ts';
 
 const app = express();
+
+app.use(express.json)
+app.use(express.urlencoded({extended: false}))
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/subscriptions', subscriptionRouter)
 
+app.use(errorMiddleware)
 
 app.get('/', (req, res) => {
     res.send('testewqeqw')
